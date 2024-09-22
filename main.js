9d4d27a2d64e543a6a6b370b28053212fa765f61
belajarcodingnih <mregi88@gmail.com>
2024-09-22 17:34:07 +0700

    add work

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ function toggleDetail (e) {
     const item = $(target).parents(".about-exp-item")
     const detail = $(item).children(".about-exp-item-detail")
 
-$(detail).slideToggle()
-
-
 if ($(target).hasClass("active")) {
     $(target).html ("More Info").removeClass("active")
 } else {
     $(target).html("less info").addClass("active")
 }
+
+$(detail).slideToggle()
+
 }
